ee4a18ef20019676845740e7ef08e3c45e840b71
delphidabbler <delphidabbler@dc8f6a02-5704-3d14-6293-46d633737b2a>
2012-05-05 08:23:38 +0000

    Created new Test folder structure in trunk for test code. Created new DUnit unit test project with a single test case for Engine.URules unit.

---
+++ Tests/Src/DUnit/YaLifeTests.dpr
@@ -0,0 +1,37 @@
+program YaLifeTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  TestRules in 'TestRules.pas',
+  Engine.URules in '..\..\..\Src\Engine.URules.pas',
+  Engine.UCommon in '..\..\..\Src\Engine.UCommon.pas',
+  UUtils in '..\..\..\Src\UUtils.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
