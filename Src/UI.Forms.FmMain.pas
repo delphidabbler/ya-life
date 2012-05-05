83217666a5cf9b01ceb595d2402bba8d5b42fed1
delphidabbler <delphidabbler@dc8f6a02-5704-3d14-6293-46d633737b2a>
2012-05-05 01:24:23 +0000

    Created a rough mock up of main window and its main controls.

--- Src/UI.Forms.FmMain.pas
+++ Src/UI.Forms.FmMain.pas
@@ -3,16 +3,37 @@ unit UI.Forms.FmMain;
 interface
 
 uses
-  UI.Forms.FmBase;
+  UI.Forms.FmBase, Classes, ActnList, StdActns, Controls, ToolWin, ActnMan,
+  ActnCtrls, ActnMenus, ImgList, PlatformDefaultStyleActnCtrls, ComCtrls, Forms,
+  ExtCtrls, StdCtrls;
 
 type
   TMainForm = class(TBaseForm)
+    actionManager: TActionManager;
+    enabledImages: TImageList;
+    disabledImages: TImageList;
+    mainMenu: TActionMainMenuBar;
+    actExit: TFileExit;
+    statusBar: TStatusBar;
+    pnlLibrary: TPanel;
+    splitterLibrary: TSplitter;
+    pnlRight: TPanel;
+    splitterTools: TSplitter;
+    scrollBoxGrid: TScrollBox;
+    paintBoxGrid: TPaintBox;
+    mainToolbar: TActionToolBar;
+    lblLibrary: TLabel;
+    pnlGroupLibrary: TCategoryPanelGroup;
+    catPanel1: TCategoryPanel;
+    catPanel2: TCategoryPanel;
+    catPanel3: TCategoryPanel;
   end;
 
 var
   MainForm: TMainForm;
 
 implementation
+
 
 {$R *.dfm}
 
