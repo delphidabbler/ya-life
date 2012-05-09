eee62b02c0cf53657ba682797841b2e1dfe44249
delphidabbler <delphidabbler@dc8f6a02-5704-3d14-6293-46d633737b2a>
2012-05-09 09:57:32 +0000

    New DOSLineBreaks routine.

--- Src/UUtils.pas
+++ Src/UUtils.pas
@@ -6,6 +6,8 @@ function SplitStr(const S, Delim: string; out S1, S2: string): Boolean;
 
 function StripEOL(const S: string): string;
 
+function DOSLineBreaks(const S: string): string;
+
 implementation
 
 uses
@@ -39,6 +41,15 @@ begin
   Result := ReplaceStr(S, #13#10, ' ');
   Result := ReplaceStr(Result, #13, ' ');
   Result := ReplaceStr(Result, #10, ' ');
+end;
+
+function DOSLineBreaks(const S: string): string;
+begin
+  // need to replace DOS line ends with LF to start with so as not to replace
+  // CRLF twice
+  Result := ReplaceStr(S, #13#10, #10);
+  Result := ReplaceStr(Result, #13, #10);
+  Result := ReplaceStr(Result, #10, #13#10);
 end;
 
 end.
